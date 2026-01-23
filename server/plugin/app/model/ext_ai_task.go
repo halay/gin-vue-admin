4d644be4bef90da80191586eaf79f86f145fe0d5
goelea <goelea@google.com>
2026-01-23 10:42:28 +0800

    调整模型字段长度

--- server/plugin/app/model/ext_ai_task.go
+++ server/plugin/app/model/ext_ai_task.go
@@ -15,7 +15,7 @@ type ExtAiTask struct {
 	Options     string    `json:"options" form:"options" gorm:"comment:任务参数;column:options;size:2048;"`         //任务参数
 	Type        string    `json:"type" form:"type" gorm:"comment:任务类型;column:type;size:48;"`                    //任务类型
 	Status      string    `json:"status" form:"status" gorm:"comment:任务状态;column:status;size:48;"`              //任务状态
-	Result      string    `json:"result" form:"result" gorm:"comment:任务结果;column:result;size:2048;"`
+	Result      string    `json:"result" form:"result" gorm:"comment:任务结果;column:result;size:4096;"`
 	Description string    `json:"description" form:"description" gorm:"comment:备注说明;column:description;size:1024;"` //备注说明
 }
 
